74594d2fb772685208590b80e034552e782474da
qiwi06 <alice.olsson06@proton.me>
2025-05-12 12:54:04 +0200

    Välja vilka tärningar att spara

--- spel/yatzy.py
+++ spel/yatzy.py
@@ -1,12 +1,19 @@
 import random
-def slå_tärningar():
-    antal_tärningar=0
+def slå_tärningar(hurmånga_tärningar):
+    antal_tärningar=5-hurmånga_tärningar
     mina_tärningar = []
     while antal_tärningar<5:
         mina_tärningar.append(random.randint(1, 6))
         antal_tärningar=antal_tärningar+1
     return mina_tärningar
-print(slå_tärningar())
-
+print(slå_tärningar(5))
+sparade_tärningar = []
+tärningar= input("Vilka tärningar vill du spara?")
+print(tärningar)
+sparade_tärningar = tärningar.split()
+print(sparade_tärningar)
+resterande_tärningar = slå_tärningar(5-len(sparade_tärningar))
+print(resterande_tärningar)
+tärningar= input("Vilka tärningar vill du spara?")
 
 
